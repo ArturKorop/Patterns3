990397c6a62c6f10e0530fe81f13778ad699eeea
Artur Korop <rubkaarchi@gmail.com>
2015-02-23 15:29:16 +0200

    TemplateMethod

---
+++ Core/TemplateMethod/CaffeineBeverage.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+
+namespace Core.TemplateMethod
+{
+    public abstract class CaffeineBeverage
+    {
+        public void PrepareRecipe()
+        {
+            this.BoilWater();
+            this.Brew();
+            this.PourInCup();
+            this.AddCondiments();
+        }
+
+        protected abstract void Brew();
+
+        protected abstract void AddCondiments();
+
+        protected void BoilWater()
+        {
+            "Boiling water".P();
+        }
+
+        protected void PourInCup()
+        {
+            "Poruing into cup".P();
+        }
+    }
+
+    public class Tea : CaffeineBeverage
+    {
+        protected override void Brew()
+        {
+            "Steeping the tea".P();
+        }
+
+        protected override void AddCondiments()
+        {
+            "Adding lemon".P();
+        }
+    }
+
+    public class Coffee : CaffeineBeverage
+    {
+        protected override void Brew()
+        {
+            "Dripping Coffee through filter".P();
+        }
+
+        protected override void AddCondiments()
+        {
+            "Adding Sugar and Milk".P();
+        }
+    }
+
+}
---
+++ Core/TemplateMethod/TemplateMethod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+
+namespace Core.TemplateMethod
+{
+    [RunAttribute(true, 9)]
+    public class TemplateMethod : IRunner
+    {
+        public void Run()
+        {
+            var drinks = new List<CaffeineBeverage> { new Tea(), new Coffee() };
+            foreach (var drink in drinks)
+            {
+                drink.PrepareRecipe();
+            }
+        }
+    }
+}
