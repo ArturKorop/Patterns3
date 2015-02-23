9facbfe8e73046978a8f9a0993cb075167023eac
Artur Korop <rubkaarchi@gmail.com>
2015-02-23 12:03:27 +0200

    Command

---
+++ Core/Command/Light.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+
+namespace Core.Command
+{
+    public class Light
+    {
+        public void On()
+        {
+            "Light on".P();
+        }
+
+        public void Off()
+        {
+            "Light off".P();
+        }
+    }
+
+    public class LightOnCommand : ICommand
+    {
+        private Light light;
+
+        public LightOnCommand(Light target)
+        {
+            this.light = target;
+        }
+
+        public void Execute()
+        {
+            this.light.On();
+        }
+    }
+
+    public class LightOffCommand : ICommand
+    {
+        private Light light;
+        
+        public LightOffCommand(Light target)
+        {
+            this.light = target;
+        }       
+
+        public void Execute()
+        {
+            this.light.Off();
+        }
+    }
+}
---
+++ Core/Command/RemoteControl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+
+namespace Core.Command
+{
+    public class RemoteControl
+    {
+        public Tuple<ICommand, ICommand>[] Commands = new Tuple<ICommand,ICommand>[7];
+
+        public void SetCommand(int number, ICommand onCommand, ICommand offCommand)
+        {
+            this.Commands[number] = new Tuple<ICommand, ICommand>(onCommand, offCommand);
+        }
+
+        public void PushOn(int number)
+        {
+            this.Commands[number].Item1.Execute();
+        }
+
+        public void PushOff(int number)
+        {
+            this.Commands[number].Item2.Execute();
+        }
+    }
+}
